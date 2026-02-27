e3373e92d5d9b8c772ea00ab774dacc8181bce1e
Spokay <hugobressange@gmail.com>
2026-02-27 14:59:05 +0100

    fea: initializing the express backend

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from 'express';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
